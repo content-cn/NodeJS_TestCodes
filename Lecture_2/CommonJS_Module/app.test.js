5f210423f995f4b8d79a5091ab7ba0a642d23c36
Nishant-Bhosale <nishantbhosale244@gmail.com>
2023-04-28 16:57:49 +0530

    Made changes

--- Lecture_2/CommonJS_Module/app.test.js
+++ Lecture_2/CommonJS_Module/app.test.js
@@ -1,7 +1,19 @@
 const Solution = require("./app");
+const math = require("./math");
 const consoleSpy = jest.spyOn(console, "log");
 
 describe("Required data is logged in the console", () => {
+  describe("math module", () => {
+    it("exports an object with sum and mean functions", () => {
+      expect(math).toEqual(
+        expect.objectContaining({
+          sum: expect.any(Function),
+          mean: expect.any(Function),
+        })
+      );
+    });
+  });
+
   it("should log the correct sum and mean in the console", () => {
     Solution();
     expect(consoleSpy).toHaveBeenCalledTimes(2);
