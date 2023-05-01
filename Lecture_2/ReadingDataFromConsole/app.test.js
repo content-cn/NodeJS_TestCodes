7c02a3432b0de4f1e498648a98863446bea37fbb
Nishant-Bhosale <nishantbhosale244@gmail.com>
2023-05-01 11:00:59 +0530

    Added new test

--- Lecture_2/ReadingDataFromConsole/app.test.js
+++ Lecture_2/ReadingDataFromConsole/app.test.js
@@ -1,18 +1,31 @@
 const readline = require("readline");
+const Solution = require("./src/index");
 
-jest.spyOn(readline, "createInterface").mockReturnValue({
-  question: jest
-    .fn()
-    .mockImplementationOnce((question, cb) => cb("5"))
-    .mockImplementationOnce((question, cb) => cb("10")),
-  close: jest.fn(),
-});
+const createInterfaceSpy = jest
+  .spyOn(readline, "createInterface")
+  .mockReturnValue({
+    question: jest
+      .fn()
+      .mockImplementationOnce((question, cb) => cb("5"))
+      .mockImplementationOnce((question, cb) => cb("10")),
+    close: jest.fn(),
+  });
+
+const consoleSpy = jest.spyOn(console, "log");
 
 describe("get max", () => {
   it("returns the maximun of two numbers", () => {
-    const consoleSpy = jest.spyOn(console, "log");
+    Solution();
 
-    expect(consoleSpy).toHaveBeenCalledWith();
+    expect(createInterfaceSpy).toHaveBeenCalledTimes(1);
+    expect(
+      createInterfaceSpy.mock.results[0].value.question
+    ).toHaveBeenCalledTimes(2);
+
+    expect(consoleSpy.mock.calls[0][0]).toMatch(
+      /the maximum of the two numbers is: \d+/i
+    );
+    expect(consoleSpy).toHaveBeenCalledTimes(1);
     readline.createInterface.mockRestore();
   });
 });
--- Lecture_2/ReadingDataFromConsole/src/index.js
+++ Lecture_2/ReadingDataFromConsole/src/index.js
@@ -1,22 +1,27 @@
 // Import the readline module
 const readline = require("readline");
 
-// Create a readline interface
-const interface = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
-
-// Prompt the user for input
-interface.question("Enter the first number: ", (n1) => {
-  interface.question("Enter the second number: ", (n2) => {
-    const maxi = Math.max(Number(n1), Number(n2));
-    console.log(`The maximum of the two numbers is: ${maxi}`);
-
-    // Close the readline interface
-    interface.close();
+const Solution = () => {
+  // Create a readline interface
+  const qInterface = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
   });
-});
+
+  // Prompt the user for input
+  qInterface.question("Enter the first number: ", (n1) => {
+    qInterface.question("Enter the second number: ", (n2) => {
+      const maxi = Math.max(Number(n1), Number(n2));
+      console.log(`The maximum of the two numbers is: ${maxi}`);
+
+      // Close the readline interface
+      qInterface.close();
+    });
+  });
+};
+
+Solution();
+module.exports = Solution;
 
 // ## Explanation
 // This code creates an interface for user input and output using the readline module in Node.js.
